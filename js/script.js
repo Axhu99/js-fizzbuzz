9a4421a1bb6366ed7e200088f42cbeaf94a34148
Axhu99 <xhuliodreni@icloud.com>
2023-12-20 16:05:34 +0100

    scaletta n^1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* 
+    1- prendere l'id
+    2- fra un ciclo for che parte da 1 a 100
+    3- fare 3 controlli:
+        1- divisibile per 3
+        2- divisibile per 5
+        3- divisibile per entrambi
+    4- inserire il numero
+    4b- in base hai controlli aggiungere una diversa classe
+    5- stampare tutto
+*/
